8a7ef8106dadf7474d1880c40c7bf63be4b8241b
mathieuboudreau <emb6150@gmail.com>
2016-12-20 15:55:57 -0330

    Bug fix: rename package names in import

--- electropy/__init__.py
+++ electropy/__init__.py
@@ -1,2 +1,2 @@
-from electrodynamics.elementary.particle import *
-from electrodynamics.elementary.charge import *
+from electropy.elementary.particle import *
+from electropy.elementary.charge import *
