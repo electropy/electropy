f66433e4d4d6c118165d6422874e64a439e87e1d
mathieuboudreau <emb6150@gmail.com>
2016-12-21 11:37:27 -0330

    Add unit tests

--- test/test_Particle.py
+++ test/test_Particle.py
@@ -29,3 +29,17 @@ class ParticleTest(unittest.TestCase):
 
     def test_that_3D_numpy_array_returns_an_object_of_type_Particle(self):
         assert(isinstance(ep.Particle(np.array(self.positionList)), ep.Particle))
+
+    #  Object property initialization tests
+
+    def test_that_nparr_pos_attribute_is_not_equal_to_list_after_object_creation_using_list(self):
+        testObj = ep.Particle(self.positionList)
+        assert(~isinstance(testObj.pos, list))
+
+    def test_that_list_pos_input_matches_the_nparr_pos_attribute_after_object_creation(self):
+        testObj = ep.Particle(self.positionList)
+        np.testing.assert_allclose(testObj.pos, np.array(self.positionList))
+
+    def test_that_nparray_pos_input_matches_the_nparr_pos_attribute_after_object_creation(self):
+        testObj = ep.Particle(np.array(self.positionList))
+        np.testing.assert_allclose(testObj.pos, np.array(self.positionList))
